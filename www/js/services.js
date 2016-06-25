b9ece96e389373b3f3801a19aad7c710618d5655
MatthGitHub <matiasbenditti@gmail.com>
2016-06-25 20:08:15 -0300

    Agrego sector restaurantes y busqueda de restaurantes cercanos

--- www/js/services.js
+++ www/js/services.js
@@ -6,29 +6,14 @@ angular.module('starter.services', [])
   // Some fake testing data
   var chats = [{
     id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'img/ben.png'
+    name: 'Familia Weiss',
+    lastText: 'Valte O connor 401, San Carlos de Bariloche, Argentina54-294-443-5789',
+    face: 'img/familiaWeis.jpg'
   }, {
     id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'img/max.png'
-  }, {
-    id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'img/adam.jpg'
-  }, {
-    id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'img/perry.png'
-  }, {
-    id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'img/mike.png'
+    name: 'Friends',
+    lastText: 'Bartolome Mitre 300, San Carlos de Bariloche, Argentina 5423700',
+    face: 'img/friends.jpg'
   }];
 
   return {
