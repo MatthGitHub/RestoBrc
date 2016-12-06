1732b555c83751c8f9cd70b6c1342ac37ca8dff7
MatthGitHub <matiasbenditti@gmail.com>
2016-12-06 19:33:37 -0300

    Agrego llave para crear apk

--- www/js/services.js
+++ www/js/services.js
@@ -4,47 +4,75 @@ angular.module('starter.services', [])
   // Might use a resource here that returns a JSON array
 
   // Some fake testing data
-  var restorantes = [{
-    id: 0,
-    name: 'La Fonda Del Tio',
-    direccion: 'Mitre 1190, San Carlos de Bariloche, Río Negro',
-    avatar: 'img/fonda_del_tio.jpg',
-    telefono: '0294 443-5011',
-    horario: '12-15:30 & 20-00',
-    tipo: 'Parrilla'
-  }, {
-    id: 1,
-    name: 'Friends',
-    direccion: 'Mitre 302, San Carlos de Bariloche, Río Negro',
-    avatar: 'img/friends.jpg',
-    telefono: '0294 442-3700',
-    horario: '08-06',
-    tipo: 'Variado'
-  }, {
-    id: 2,
-    name: 'Jauja',
-    direccion: 'Elflein 148, 8400 San Carlos de Bariloche, Río Negro',
-    avatar: 'img/jauja.jpg',
-    telefono: '0294 442-2952',
-    horario: '12-15 & 19-00',
-    tipo: 'Variado'
-  }, {
-    id: 3,
-    name: 'La Marmite',
-    direccion: 'Mitre 329, 8400 San Carlos de Bariloche, Río Negro',
-    avatar: 'img/la_marmite.jpg',
-    telefono: '0294 443-2198',
-    horario: 'Consultar',
-    tipo: 'Parrilla'
-  }, {
-    id: 4,
-    name: 'Familia Weis',
-    direccion: 'Vice Almte O Connor 1120, San Carlos de Bariloche, Río Negro',
-    avatar: 'img/familiaWeis.jpg',
-    telefono: 'No disponible',
-    horario: '09-20',
-    tipo: 'Pasta'
-  }];
+  // var restorantes = [{
+  //   id: 0,
+  //   name: 'La Fonda Del Tio',
+  //   direccion: 'Mitre 1190, San Carlos de Bariloche, Río Negro',
+  //   avatar: 'img/fonda_del_tio.jpg',
+  //   telefono: '0294 443-5011',
+  //   horario: '12-15:30 & 20-00',
+  //   tipo: 'Parrilla'
+  // }, {
+  //   id: 1,
+  //   name: 'Friends',
+  //   direccion: 'Mitre 302, San Carlos de Bariloche, Río Negro',
+  //   avatar: 'img/friends.jpg',
+  //   telefono: '0294 442-3700',
+  //   horario: '08-06',
+  //   tipo: 'Variado'
+  // }, {
+  //   id: 2,
+  //   name: 'Jauja',
+  //   direccion: 'Elflein 148, 8400 San Carlos de Bariloche, Río Negro',
+  //   avatar: 'img/jauja.jpg',
+  //   telefono: '0294 442-2952',
+  //   horario: '12-15 & 19-00',
+  //   tipo: 'Variado'
+  // }, {
+  //   id: 3,
+  //   name: 'La Marmite',
+  //   direccion: 'Mitre 329, 8400 San Carlos de Bariloche, Río Negro',
+  //   avatar: 'img/la_marmite.jpg',
+  //   telefono: '0294 443-2198',
+  //   horario: 'Consultar',
+  //   tipo: 'Parrilla'
+  // }, {
+  //   id: 4,
+  //   name: 'Familia Weis',
+  //   direccion: 'Vice Almte O Connor 1120, San Carlos de Bariloche, Río Negro',
+  //   avatar: 'img/familiaWeis.jpg',
+  //   telefono: 'No disponible',
+  //   horario: '09-20',
+  //   tipo: 'Pasta'
+  // }];
+
+  var db = firebase.database();
+  var ref = db.ref('/restaurantes/');
+  var restorantes = [];
+
+  var restosref = ref.on('value', function(data) {
+    var i = 0;
+    data.forEach(function(resto) {
+      i++;
+      var arr2 = {};
+      console.log('resto a %s el en %s ',resto.val().nombre, resto.val().calle);
+
+      arr2 = {
+        id:resto.val().restoId,
+        nombre: resto.val().nombre,
+        telefono: resto.val().telefono,
+        calle: resto.val().calle,
+        numero: resto.val().numero,
+        ciudad: resto.val().ciudad,
+        provincia: resto.val().provincia,
+        usuarioId: resto.val().usuarioId
+       };
+      restorantes.push(arr2);
+   });
+  });
+
+
+
 
   console.log(restorantes);
 
@@ -58,7 +86,7 @@ angular.module('starter.services', [])
     },
     get: function(restoranteId) {
       for (var i = 0; i < restorantes.length; i++) {
-        if (restorantes[i].id === parseInt(restoranteId)) {
+        if (restorantes[i].id === (restoranteId)) {
           return restorantes[i];
         }
       }
